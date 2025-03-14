a14092098157e7b0f5c13582452ee1a391187d2a
Vidyashree <vidyas69373@gmail.com>
2025-03-14 14:35:31 +0530

    first commit

---
+++ src/main/java/com/example/ncode/GenerateTestCaseToolWindowFactory.java
@@ -0,0 +1,20 @@
+package com.example.ncode;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
+
+public class GenerateTestCaseToolWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        GenerateTestCaseFactoryContent content = new GenerateTestCaseFactoryContent();
+
+        ContentFactory contentFactory = ContentFactory.getInstance();
+        Content toolWindowContent = contentFactory.createContent(content.getPanel(), "", false);
+        toolWindow.getContentManager().addContent(toolWindowContent);
+    }
+}
---
+++ src/main/java/com/example/ncode/GoogleThis.java
@@ -0,0 +1,25 @@
+package com.example.ncode;
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.NotNull;
+
+public class GoogleThis extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e){
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        BrowserUtil.browse("https://www.google.com/search?q="+selectedText);
+}
+    public void update(AnActionEvent e) {
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+
+}
